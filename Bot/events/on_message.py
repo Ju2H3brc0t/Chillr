c55321b4fde8c307d0798ac30bee266685644556
Ju2H3brc0t <164507976+Ju2H3brc0t@users.noreply.github.com>
2025-01-25 11:23:45 +0100

    fix: Replacing "discord.get_channel" with "self.bot.get_channel" in the "on_message method"

--- Bot/events/on_message.py
+++ Bot/events/on_message.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 import asyncio
 
@@ -6,19 +7,23 @@ class on_msg(commands.Cog):
         self.bot = bot
     
     @commands.Cog.listener()
-    async def on_message(bot, message):
+    async def on_message(self, message):
+        bot_channel = self.bot.get_channel(118160135961976965)
+        bot_staff_channel = self.bot.get_channel(1122777165331693678)
+        logs_channel = self.bot.get_channel(138880792009900082)
         print(f"{message} sended in {message.channel}")
+        print(f"{message.channel_id:}")
         try:
-            if message.channel == bot.get_channel(1118160135961976965):
+            if message.channel == bot_channel:
                 try:
                     await asyncio.sleep(180)
                     await message.delete()
                 except Exception as e:
                     print(f"A error has occured while deleting message: {e}")
-            elif message.channel == bot.get_channel(1122777165331693678):
+            elif message.channel == bot_staff_channel:
                 await asyncio.sleep(3600)
                 await message.delete()
-            elif message.channel == bot.get_channel(1138880792009900082):
+            elif message.channel == logs_channel:
                 await asyncio.sleep(604800)
                 await message.delete()
         except Exception as e:
