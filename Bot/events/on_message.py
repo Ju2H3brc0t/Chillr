ce72f2bc396d34adc0ef66e2683e76b02ff219cd
Ju2H3brc0t <164507976+Ju2H3brc0t@users.noreply.github.com>
2025-01-25 11:06:01 +0100

    debug: Added print and try/except

--- Bot/events/on_message.py
+++ Bot/events/on_message.py
@@ -7,15 +7,22 @@ class on_msg(commands.Cog):
     
     @commands.Cog.listener()
     async def on_message(bot, message):
-        if message.channel == bot.get_channel(1118160135961976965):
-            await asyncio.sleep(180)
-            await message.delete()
-        elif message.channel == bot.get_channel(1122777165331693678):
-            await asyncio.sleep(3600)
-            await message.delete()
-        elif message.channel == bot.get_channel(1138880792009900082):
-            await asyncio.sleep(604800)
-            await message.delete()
+        print(f"{message} sended in {message.channel}")
+        try:
+            if message.channel == bot.get_channel(1118160135961976965):
+                try:
+                    await asyncio.sleep(180)
+                    await message.delete()
+                except Exception as e:
+                    print(f"A error has occured while deleting message: {e}")
+            elif message.channel == bot.get_channel(1122777165331693678):
+                await asyncio.sleep(3600)
+                await message.delete()
+            elif message.channel == bot.get_channel(1138880792009900082):
+                await asyncio.sleep(604800)
+                await message.delete()
+        except Exception as e:
+            print(f"A error has occured while sorting the rooms: {e}")
     
 async def setup(bot):
     await bot.add_cog(on_msg(bot))
