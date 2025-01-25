a1e9b961ecb75101950f27bf6b490ad832fceebf
Ju2H3brc0t <164507976+Ju2H3brc0t@users.noreply.github.com>
2025-01-25 10:37:16 +0100

    feat: added automatic deletion of messages in some rooms

---
+++ Bot/event/on_message.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+import asyncio
+
+class on_msg(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_message(bot, message):
+        if message.channel == bot.get_channel(1118160135961976965):
+            await asyncio.sleep(180)
+            await message.delete()
+        elif message.channel == bot.get_channel(1122777165331693678):
+            await asyncio.sleep(3600)
+            await message.delete()
+        elif message.channel == bot.get_channel(1138880792009900082):
+            await asyncio.sleep(604800)
+            await message.delete()
