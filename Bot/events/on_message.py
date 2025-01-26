3031bd2b36532e991b578aa49f5eb9edcfed80a0
Ju2H3brc0t <164507976+Ju2H3brc0t@users.noreply.github.com>
2025-01-26 10:27:28 +0100

    refactor: implement configuration file for message handling parameters in on_message method

--- Bot/events/on_message.py
+++ Bot/events/on_message.py
@@ -1,5 +1,27 @@
 from discord.ext import commands
 import asyncio
+import yaml
+import os
+
+def load_config(config_file="event_config.yaml"):
+    dir = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(dir, config_file)
+
+    if not os.path.exists(path):
+        raise FileNotFoundError("Configuration file not found")
+
+    with open(path, "r") as file:
+        return yaml.safe_load(file)
+    
+config = load_config()
+
+bot_channel = config['event']['on_message']['channel_id']['bot']
+staff_bot_channel = config['event']['on_message']['channel_id']['bot_staff']
+log_channel = config['event']['on_message']['channel_id']['log']
+
+bot_sleep = config['event']['on_message']['sleep_time']['bot']
+staff_bot_sleep = config['event']['on_message']['sleep_time']['bot_staff']
+log_sleep = config['event']['on_message']['sleep_time']['log']
 
 class on_msg(commands.Cog):
     def __init__(self, bot):
@@ -8,18 +30,15 @@ class on_msg(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         message_channel = message.channel.id
-        bot_channel = 1118160135961976965
-        staff_bot_channel = 1122777165331693678
-        log_channel = 1138880792009900082
 
         if message_channel == bot_channel:
-            await asyncio.sleep(180)
+            await asyncio.sleep(bot_sleep)
             message.delete()
         elif message_channel == staff_bot_channel:
-            await asyncio.sleep(3600)
+            await asyncio.sleep(staff_bot_channel)
             await message.delete()
         elif message_channel == log_channel:
-            await asyncio.sleep(604800)
+            await asyncio.sleep(log_sleep)
             await message.delete()
 
     
