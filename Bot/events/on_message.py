674af9f9fa1d235f1e1c1fa5d04b0a8744ac2142
Ju2H3brc0t <164507976+Ju2H3brc0t@users.noreply.github.com>
2025-01-26 10:12:03 +0100

    chore: streamline message deletion logic in on_message method

--- Bot/events/on_message.py
+++ Bot/events/on_message.py
@@ -1,4 +1,3 @@
-import discord
 from discord.ext import commands
 import asyncio
 
@@ -12,24 +11,17 @@ class on_msg(commands.Cog):
         bot_channel = 1118160135961976965
         staff_bot_channel = 1122777165331693678
         log_channel = 1138880792009900082
-        print(message.channel.id)
-        print(bot_channel)
-        try:
-            print("Starting to sort the rooms.")
-            if message_channel == bot_channel:
-                print("Starting message deletion.")
-                await asyncio.sleep(5)
-                await message.delete()
-            elif message_channel == staff_bot_channel:
-                await asyncio.sleep(3600)
-                await message.delete()
-            elif message_channel == log_channel:
-                await asyncio.sleep(604800)
-                await message.delete()
-            else:
-                print("No rooms to sort.")
-        except Exception as e:
-            print(f"An error has occurred while sorting the rooms: {e}")
+
+        if message_channel == bot_channel:
+            await asyncio.sleep(180)
+            message.delete()
+        elif message_channel == staff_bot_channel:
+            await asyncio.sleep(3600)
+            await message.delete()
+        elif message_channel == log_channel:
+            await asyncio.sleep(604800)
+            await message.delete()
+
     
 async def setup(bot):
     await bot.add_cog(on_msg(bot))
