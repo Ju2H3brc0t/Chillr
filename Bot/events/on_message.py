b889ddd5fcff2be860f1ef5d474fa35a68fbfa7c
Ju2H3brc0t <164507976+Ju2H3brc0t@users.noreply.github.com>
2025-01-26 10:05:38 +0100

    fix: update channel IDs for message handling in on_message method

--- Bot/events/on_message.py
+++ Bot/events/on_message.py
@@ -9,9 +9,9 @@ class on_msg(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         message_channel = message.channel.id
-        bot_channel = 118160135961976965
+        bot_channel = 1118160135961976965
         staff_bot_channel = 1122777165331693678
-        log_channel = 138880792009900082
+        log_channel = 1138880792009900082
         print(message.channel.id)
         print(bot_channel)
         try:
@@ -26,6 +26,8 @@ class on_msg(commands.Cog):
             elif message_channel == log_channel:
                 await asyncio.sleep(604800)
                 await message.delete()
+            else:
+                print("No rooms to sort.")
         except Exception as e:
             print(f"An error has occurred while sorting the rooms: {e}")
     
