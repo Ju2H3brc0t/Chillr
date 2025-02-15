780092f676b847ee103867a8f9dc5638d60e0ec4
Abr1cOt <164507976+Ju2H3brc0t@users.noreply.github.com>
2025-02-15 13:51:10 +0100

    feat: add channel IDs for bot, bot staff, and log channels in stop command

--- Bot/commands/stop.py
+++ Bot/commands/stop.py
@@ -18,6 +18,9 @@ def load_config(config_file="command_config.yaml"):
 config = load_config()
 
 counting_channel = config['command']['stop']['channel_id']['counting']
+bot_channel = config['command']['stop']['channel_id']['bot']
+bot_staff_channel = config['command']['stop']['channel_id']['bot_staff']
+log_channel = config['command']['stop']['channel_id']['log']
 
 class stop(commands.Cog):
     def __init__(self, bot):
@@ -26,13 +29,22 @@ class stop(commands.Cog):
     @app_commands.command(name="stop", description="Stop the bot.")
     async def stop(self, interaction: discord.Interaction):
         counting_channel_id = self.bot.get_channel(counting_channel)
+        bot_channel_id = self.bot.get_channel(bot_channel)
+        bot_staff_channel_id = self.bot.get_channel(bot_staff_channel)
+        log_channel_id = self.bot.get_channel(log_channel)
+        
         await interaction.response.send_message("Stopping the bot...", ephemeral=True)
+        
         for role in interaction.guild.roles:
             if role.is_default():
                 continue
             await counting_channel_id.set_permissions(role, send_messages=False)
         await self.bot.close()
         sys.exit(0)
+
+        await bot_channel_id.purge(limit=100)
+        await bot_staff_channel_id.purge(limit=100)
+        await log_channel_id.purge(limit=100)
 
 async def setup(bot):
     await bot.add_cog(stop(bot))
