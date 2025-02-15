b451b3f65e9f45a5b3792809d060b9febad2bac9
Abr1cOt <164507976+Ju2H3brc0t@users.noreply.github.com>
2025-02-15 11:15:47 +0100

    fix: enhance say command to handle messages without a specified channel

--- Bot/commands/say.py
+++ Bot/commands/say.py
@@ -9,7 +9,12 @@ class say(commands.Cog):
     @app_commands.command(name="say", description="Make the bot say something.")
     @app_commands.describe(message="The message you want the bot to say.", channel="The channel you want the bot to say the message in.")
     async def say(self, interaction: discord.Interaction, message: str, channel: discord.TextChannel = None):
-        await channel.send(message)
+        if channel is None:
+            await interaction.response.send_message(message)
+            await interaction.response.send_message(f"Sent {message} to {interaction.channel.mention}.", ephemeral=True)
+        else:
+            await channel.send(message)
+            await interaction.response.send_message(f"Sent {message} to {channel.mention}.", ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(say(bot))
