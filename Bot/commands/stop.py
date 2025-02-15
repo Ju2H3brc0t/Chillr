da81be2f8f6e7c10a027c31a92b6618a7dd5a3c5
Abr1cOt <164507976+Ju2H3brc0t@users.noreply.github.com>
2025-02-15 14:10:11 +0100

    feat: add bot owner/server owner ID check to stop command for enhanced security

--- Bot/commands/stop.py
+++ Bot/commands/stop.py
@@ -17,6 +17,8 @@ def load_config(config_file="command_config.yaml"):
 
 config = load_config()
 
+bot_owner_id = config['command']['stop']['user_id']['bot_owner']
+
 counting_channel = config['command']['stop']['channel_id']['counting']
 bot_channel = config['command']['stop']['channel_id']['bot']
 bot_staff_channel = config['command']['stop']['channel_id']['bot_staff']
@@ -27,26 +29,32 @@ class stop(commands.Cog):
         self.bot = bot
     
     @app_commands.command(name="stop", description="Stop the bot.")
-    async def stop(self, interaction: discord.Interaction):
+    async def stop(self, interaction: discord.Interaction, bot_owner_id):
+        user_id = interaction.user.id
+        server_owner_id = interaction.guild.owner_id
+
         counting_channel_id = self.bot.get_channel(counting_channel)
         bot_channel_id = self.bot.get_channel(bot_channel)
         bot_staff_channel_id = self.bot.get_channel(bot_staff_channel)
         log_channel_id = self.bot.get_channel(log_channel)
         
-        await interaction.response.send_message("Stopping the bot...", ephemeral=True)
-        print("Stopping the bot...")
+        if user_id == bot_owner_id or user_id == server_owner_id:
+            await interaction.response.send_message("Stopping the bot...", ephemeral=True)
+            print("Stopping the bot...")
         
-        for role in interaction.guild.roles:
-            if role.is_default():
-                continue
-            await counting_channel_id.set_permissions(role, send_messages=False)
+            for role in interaction.guild.roles:
+                if role.is_default():
+                    continue
+                await counting_channel_id.set_permissions(role, send_messages=False)
         
-        await bot_channel_id.purge(limit=100)
-        await bot_staff_channel_id.purge(limit=100)
-        await log_channel_id.purge(limit=100)
+            await bot_channel_id.purge(limit=100)
+            await bot_staff_channel_id.purge(limit=100)
+            await log_channel_id.purge(limit=100)
 
-        await self.bot.close()
-        sys.exit(0)
+            await self.bot.close()
+            sys.exit(0)
+        else:
+            await interaction.response.send_message("", ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(stop(bot))
