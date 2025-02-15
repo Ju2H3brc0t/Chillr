3c61ac414d9d525d587c65a7205ebdd89f6bfa17
Abr1cOt <164507976+Ju2H3brc0t@users.noreply.github.com>
2025-02-15 11:59:15 +0100

    feat: enhance stop command to include channel permissions and load configuration from file

--- Bot/commands/stop.py
+++ Bot/commands/stop.py
@@ -2,14 +2,34 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import sys
+import os
+import yaml
+
+def load_config(config_file="command_config.yaml"):
+    dir = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(dir, config_file)
+
+    if not os.path.exists(path):
+        raise FileNotFoundError("Configuration file not found")
+
+    with open(path, "r") as file:
+        return yaml.safe_load(file)
+
+config = load_config()
+
+counting_channel = config['command']['stop']['channel_id']['counting']
 
 class stop(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
     @app_commands.command(name="stop", description="Stop the bot.")
-    async def stop(self, interaction: discord.Interaction):
+    async def stop(self, ctx, interaction: discord.Interaction, channel: counting_channel):
         await interaction.response.send_message("Stopping the bot...", ephemeral=True)
+        for role in ctx.guild.roles:
+            if role.is_default():
+                continue
+            await channel.set_permissions(role, send_messages=False)
         await self.bot.close()
         sys.exit(0)
 
