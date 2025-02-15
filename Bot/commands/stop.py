657c6af1593353d7c3bdeb83da9e8b67e61e491b
Abr1cOt <164507976+Ju2H3brc0t@users.noreply.github.com>
2025-02-15 14:19:50 +0100

    debug add error handling in stop command

--- Bot/commands/stop.py
+++ Bot/commands/stop.py
@@ -39,23 +39,27 @@ class stop(commands.Cog):
         bot_staff_channel_id = self.bot.get_channel(bot_staff_channel)
         log_channel_id = self.bot.get_channel(log_channel)
         
-        if user_id == bot_owner_id or user_id == server_owner_id:
-            await interaction.response.send_message("Stopping the bot...", ephemeral=True)
-            print("Stopping the bot...")
+        try:
+            if user_id == bot_owner_id or user_id == server_owner_id:
+                await interaction.response.send_message("Stopping the bot...", ephemeral=True)
+                print("Stopping the bot...")
         
-            for role in interaction.guild.roles:
-                if role.is_default():
-                    continue
-                await counting_channel_id.set_permissions(role, send_messages=False)
+                for role in interaction.guild.roles:
+                    if role.is_default():
+                        continue
+                    await counting_channel_id.set_permissions(role, send_messages=False)
         
-            await bot_channel_id.purge(limit=100)
-            await bot_staff_channel_id.purge(limit=100)
-            await log_channel_id.purge(limit=100)
+                await bot_channel_id.purge(limit=100)
+                await bot_staff_channel_id.purge(limit=100)
+                await log_channel_id.purge(limit=100)
 
-            await self.bot.close()
-            sys.exit(0)
-        else:
-            await interaction.response.send_message("Sorry, only bot owner or server owner can shutdown the bot.", ephemeral=True)
+                await self.bot.close()
+                sys.exit(0)
+            else:
+                await interaction.response.send_message("Sorry, only bot owner or server owner can shutdown the bot.", ephemeral=True)
+        except Exception as e:
+            await interaction.response.send_message(f"An error has occured: {e}", ephemeral=True)
+            print(f"An error has occured: {e}")
 
 async def setup(bot):
     await bot.add_cog(stop(bot))
